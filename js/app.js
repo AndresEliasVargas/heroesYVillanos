fa11c40d9a0abe56a762d24a3f99b7bdf339630e
Andres Vargas Ramirez <andreselias.vargas@gmail.com>
2019-07-03 15:57:15 -0600

    feat
    New Villains features

--- js/app.js
+++ js/app.js
@@ -12,7 +12,10 @@ const getHeroes = () => {
 };
 
 const getVillains = () => {
-    console.log('Villains');
+    let request = new XMLHttpRequest();
+    request.open('GET', '../data/villains.json', true);
+    request.onreadystatechange = getVillainsCallBack;
+    request.send();
 };
 
 const getHeroesCallback = e => {
@@ -20,21 +23,35 @@ const getHeroesCallback = e => {
 
     if (request.readyState == XMLHttpRequest.DONE) {
         let data = JSON.parse(request.responseText);
-        console.log(data);
+        //console.log(data);
 
-        heroes = new Squad(data.active, data.formed, data.homeTown, 
-        data.secretBase, [], data.squadName);
-
-        console.log(heroes);
-
+        heroes = new Squad(data.active, data.formed, data.homeTown, data.secretBase, [], data.squadName);
         data.members.map(memberData => {
             let member = new Member(memberData.age, memberData.image, memberData.name, memberData.powers, memberData.secretIdentity);
             heroes.addMember(member);
         });
-
-        console.log(heroes);
+        //console.log(heroes);
 
         isGetHeroesComplete = true;
+        completeLoadUI();
+    };
+};
+
+const getVillainsCallBack = e => {
+    let request = e.target;
+
+    if (request.readyState == XMLHttpRequest.DONE) {
+        let data = JSON.parse(request.responseText);
+        //console.log(data);
+
+        villains = new Squad(data.active, data.formed, data.homeTown, data.secretBase, [], data.squadName);
+        data.members.map(memberData => {
+            let member = new Member(memberData.age, memberData.image, memberData.name, memberData.powers, memberData.secretIdentity);
+            villains.addMember(member);
+        });
+        //console.log(villains);
+
+        isGetVillainsComplete = true;
         completeLoadUI();
     };
 };
@@ -42,7 +59,8 @@ const getHeroesCallback = e => {
 const completeLoadUI = () => {
     if (isGetHeroesComplete && isGetVillainsComplete) {
         //TODO Load UI
-        console.log('complete load');
+        console.log(heroes);
+        console.log(villains);
     }
 };
 
